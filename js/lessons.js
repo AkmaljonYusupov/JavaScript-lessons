8d283349c0f118abc51e5d623d69647d6d60a178
Akmaljon yusupov <infoakmaljon.uz@gmail.com>
2022-10-29 12:13:01 +0500

    10-dars. Operatorlar

---
+++ js/lessons.js
@@ -0,0 +1,34 @@
+"use strict";
+
+// console.log("Akmaljon" + "Yusupov");
+
+//  incr decr
+
+// let incr = 10;
+// let descr = 10;
+
+// incr++;
+// descr--;
+// console.log(incr++);
+// console.log(descr--);
+
+// console.log(++incr);
+// console.log(--descr);
+
+// console.log(10 % 3); // Qoldiqni chiqarib beradi
+
+// console.log(5 * 5 === "25");
+
+// && = va
+// || = yoki
+
+// const isAge = true;
+// const isClose = true;
+// const isChecked = false;
+
+// console.log(isAge && isClose);
+// console.log(isAge || isClose || isChecked);
+// console.log(isAge && isClose && isChecked);
+// console.log(isAge && isClose && !isChecked);
+
+console.log(2 + 2 * 2 !== 6);
