926b85b7e5e613211bbb5647462e3583d3a6406d
Akmaljon yusupov <infoakmaljon.uz@gmail.com>
2022-10-29 10:17:39 +0500

    9-dars Interpolatsiya

---
+++ js/lessons.js
@@ -0,0 +1,13 @@
+"use strict";
+
+// const channel = prompt("What's your favorite youtube");
+// console.log("https://youtube.com/" + channel + "/videos");
+// console.log(`https://youtube.com/${channel}`);
+
+// const user = "Akmaljon";
+// const job = "Front-end developer";
+
+// console.log(`User name is ${user}, he is ${job} `);
+
+const client = prompt("Hello");
+console.log(client);
