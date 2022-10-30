6f93cf218c9340ce8834776f5311080cc2f9c2a9
Akmaljon yusupov <infoakmaljon.uz@gmail.com>
2022-10-30 15:52:53 +0500

    14-dars. Tsikl

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log("Hello, JavaScript and Mohirdev Akademiy");
+"use strict";
+
+let Num = 5;
+
+// First
+// while (startNum < 100) {
+//   console.log(startNum);
+//   startNum++;
+// }
+
+// Second
+
+// do {
+//   console.log(startNum);
+//   startNum++;
+// } while (startNum <= 5);
+
+// Third
+
+for (let i = 1; i <= 10; i++) {
+  if (i === 8) {
+    break;
+  } else {
+    console.log(i);
+  }
+}
