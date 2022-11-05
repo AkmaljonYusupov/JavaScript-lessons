6c657bbbe6682417e66acf88170fbcd8f24cbb0c
Akmaljon yusupov <infoakmaljon.uz@gmail.com>
2022-11-05 17:31:38 +0500

    23-dars. SPREAD operator

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 "use strict";
-console.log("Hello, JavaScript and Mohirdev Akademiy");
+// const arr = [1, 2, 3];
+
+// function logger(x, y, z) {
+//   console.log(x + y + z);
+// }
+// logger(...arr);
+
+// const arrs = ["a", "y"];
+// const newArr = [...arrs];
+// console.log(newArr);
+
+const numbers = {
+  x: 10,
+  y: 20,
+};
+const newNumber = { ...numbers };
+console.log(newNumber);
