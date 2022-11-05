ca370f9cdec78cab916ee0150a0e65d21de6e646
Akmaljon yusupov <infoakmaljon.uz@gmail.com>
2022-11-05 18:00:40 +0500

    24-dars. OOP asoslari, prototipga yo'naltirilgan meros

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
 "use strict";
-console.log("Hello, JavaScript and Mohirdev Akademiy");
+//  OOP asoslari, prototipga yo'naltirilgan meros
+// const firstName = "Akmaljon";
+// const str = new String(firstName);
+// console.log(typeof firstName);
+// console.log(typeof str);
+
+const car = {
+  motor: "X",
+  colors: "red",
+  isAirbag: true,
+  isSpeed: function () {
+    console.log(320);
+  },
+};
+
+const gm = {
+  isAirbag: false,
+};
+// gm.__proto__ = car;
+// console.log(gm);
+// gm.isSpeed();
+
+Object.setPrototypeOf(gm, car); // yuklab beradi
+console.log(gm);
+
+const bmw = Object.create(car); // yaratib beradi
+console.log(gm);
