1eeef2fd0855ec347e9294cd0a185aa050a108ce
Akmaljon yusupov <infoakmaljon.uz@gmail.com>
2022-11-06 21:28:39 +0500

    26-dars. Dynamic typing

--- index.js
+++ index.js
@@ -1,75 +1,30 @@
 "use strict";
 
-let seriesDB = {
-  count: 0,
-  series: {},
-  actors: {},
-  genres: [],
-  private: true,
-  startApp: function () {
-    while (
-      seriesDB.count == "" ||
-      seriesDB.count == null ||
-      isNaN(seriesDB.count)
-    ) {
-      seriesDB.count = +prompt("Nechta serial ko`rdingiz?", "");
-    }
-  },
-  showDB: function () {
-    if (!seriesDB.private) {
-      console.log(seriesDB);
-    }
-  },
-  rememberMySeries: function () {
-    let i = 0;
-    do {
-      const a = prompt("Oxirgi ko'rgan serialingiz?"),
-        b = +prompt("Nechi baxo berasiz?");
-      if (a != "" && b != "" && a != null && b !== null) {
-        seriesDB.series[a] = b;
-        console.log("done");
-      } else {
-        console.log("error");
-        i--;
-      }
-      i++;
-    } while (i < 2);
-  },
-  detectLeveSeries: function () {
-    if (seriesDB.count < 5) {
-      console.log("Kam serial ko'ripsiz");
-    } else if (seriesDB.count >= 5 && seriesDB.count < 10) {
-      console.log("Siz classik tamoshabin ekansiz");
-    } else if (seriesDB.count >= 10) {
-      console.log("Siz serialchi zvezda ekansiz");
-    } else {
-      console.log("Error");
-    }
-  },
-  writeGenres: function () {
-    // let i = 0;
-    // while (i <= 2) {
-    //   i++;
-    // }
-    let b = prompt(`Yaxshi korgan janringiz`).toLowerCase();
-    if (b === "" || b === null) {
-      console.log("Berilgan maydonni to`ldiring");
-      i--;
-    } else {
-      seriesDB.genres = b.split(", ");
-      seriesDB.genres.sort();
-    }
-    seriesDB.genres.forEach((value, index) => {
-      console.log(
-        `Sizni yaxshi ko'rgan janeringiz ${index + 1} - nomi ${value}`
-      );
-    });
-  },
-  visibleDB: function () {
-    if (seriesDB.private) {
-      seriesDB.private = false;
-    } else {
-      seriesDB.private = true;
-    }
-  },
-};
+// to string
+console.log(typeof String(4));
+
+console.log(typeof ("Akmaljon" + 24));
+const youtubeChanel = 10;
+console.log(`youtube.com/channel/` + youtubeChanel);
+
+// to Number
+// 1-)
+console.log(typeof Number("24"));
+// 2-)
+console.log(typeof +"24");
+// 3-)
+console.log(typeof parseInt(24));
+
+// to Boolean
+// 0, "", null, undefined, NaN ==> xar doim false chiqadi
+let age = 0;
+if (age) {
+  console.log("24");
+}
+age = 24;
+if (age) {
+  console.log("Akmaljon");
+}
+
+console.log(typeof Boolean("4"));
+console.log(typeof !!"4");
