d3dab1d0f1fe89e4f51db372e409653b5f774881
Akmaljon yusupov <infoakmaljon.uz@gmail.com>
2022-11-08 20:57:31 +0500

    30-dars. Xodisalar bilan ishlash

--- index.js
+++ index.js
@@ -1,2 +1,58 @@
 "use strict";
-console.log("Hello, JavaScript and Mohirdev Akademiy");
+// const btn = document.querySelector("#btn");
+// const overlay = document.querySelector(".overlay");
+// btn.onclick = function () {
+//   alert("Click");
+// };
+
+// btn.onclick = function () {
+//   alert("Click 2");
+// };
+
+// btn.addEventListener("click", () => {
+//   alert("Click 2");
+// });
+// btn.addEventListener("click", () => {
+//   alert("Click 3");
+// });
+
+// btn.addEventListener("mouseenter", (event) => {
+//   alert("Hover");
+//   event.target.remove();
+// });
+
+// const deleteElement = (event) => {
+//   event.target.remove();
+// };
+// btn.addEventListener("click", deleteElement);
+
+// let i = 0;
+// const addElement = (event) => {
+//   i++;
+//   if (i === 3) {
+//     btn.removeEventListener("click", addElement);
+//   }
+//   console.log(i);
+// };
+
+// const cb = (event) => {
+//   console.log(event.currentTarget);
+// };
+
+// btn.addEventListener("click", cb);
+// overlay.addEventListener("click", cb);
+
+const link = document.querySelector("a");
+link.addEventListener("click", (event) => {
+  event.preventDefault();
+  alert("Linka");
+});
+
+const logger = () => {
+  console.log(1);
+};
+
+const btns = document.querySelectorAll("button");
+btns.forEach((item) => {
+  item.addEventListener("click", logger);
+});
