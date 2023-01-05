ea0dcadc8fa5a9d2ac07d8893065ac926281423e
Akmaljon yusupov <infoakmaljon.uz@gmail.com>
2023-01-05 20:44:47 +0500

    35-dars. ClassList

--- js/script.js
+++ js/script.js
@@ -33,3 +33,38 @@ btns[0].addEventListener("click", () => {
 
   btns[1].classList.toggle("red");
 });
+
+// Ma`lumot classList
+const div = document.createElement("div");
+div.className = "foo";
+
+// our starting state: <div class="foo"></div>
+console.log(div.outerHTML);
+
+// use the classList API to remove and add classes
+div.classList.remove("foo");
+div.classList.add("anotherclass");
+
+// <div class="anotherclass"></div>
+console.log(div.outerHTML);
+
+// if visible is set remove it, otherwise add it
+div.classList.toggle("visible");
+
+// add/remove visible, depending on test conditional, i less than 10
+let i;
+div.classList.toggle("visible", i < 10);
+
+console.log(div.classList.contains("foo"));
+
+// add or remove multiple classes
+div.classList.add("foo", "bar", "baz");
+div.classList.remove("foo", "bar", "baz");
+
+// add or remove multiple classes using spread syntax
+const cls = ["foo", "bar"];
+div.classList.add(...cls);
+div.classList.remove(...cls);
+
+// replace class "foo" with class "bar"
+div.classList.replace("foo", "bar");
