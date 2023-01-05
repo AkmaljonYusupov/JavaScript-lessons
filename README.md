7faeb3b2f2704648cb42b5dcb1b7e3f1a111ac38
Akmaljon yusupov <infoakmaljon.uz@gmail.com>
2023-01-05 22:07:56 +0500

    36-dars. Delegatsiya xaqida batafsil

--- README.md
+++
@@ -1,4 +0,0 @@
-# JavaScript-lessons 35-dars. ClassList
-Mohirdev Academiy darsliklari: Full Stack Javascript (MERN) 1-MODUL <hr>
-<h3>ClassList elementlar xaqida =><a href="https://developer.mozilla.org/en-US/docs/Web/API/Element/classList">LINK</a></h3>
-<h3>Matches method xaqida =><a href="https://developer.mozilla.org/en-US/docs/Web/API/Element/matches">LINK</a></h3>
--- js/script.js
+++ js/script.js
@@ -1,70 +1,46 @@
 "use strict";
 
-const btns = document.querySelectorAll("button");
+const wrapper = document.querySelector(".btn-block"),
+  btns = document.querySelectorAll("button");
+// consolda Click ni chiqarib beradi ixtiyoriy elementga bosganda
+// wrapper.addEventListener("click", () => {
+//   console.log("Click");
+// });
 
-// class lar soni
-// console.log(btns[0].classList.length);
-
-// class larni chiqrib beruvchi xisoblanadi
-// console.log(btns[0].classList.item(0));
-
-// classni o`chiradi
-// btns[0].classList.remove("blue");
-
-//  class qo`shadi
-// btns[1].classList.add("blue", "some");
-// btns[0].classList.add("red");
-
-// class bor bo`lsa o`chiradi yo`q bo`lsa qo`shib qo`yadi => bor bo`lsa yo`q qiladi, yo`q bo`lsa bor qiladi
-// btns[0].classList.toggle("blue");
-
-// classni bor yoki yo`qligini tekshiradi ayni if ni ichida berilgan shartga ko`ra
-// contains('....') da berilgan class qiymatiga ko`ra
-// if (btns[0].classList.contains("red")) {
-//   console.log("yeap");
-// }
+// ona elementi orqali button tugmaga element qo`shish
+// wrapper.addEventListener("click", (e) => {
+//     // button tugmani bosilganda
+//   if (e.target && e.target.tagName == "BUTTON") {
+//     console.log("Click me");
+//   }
+// });
 
 btns[0].addEventListener("click", () => {
-  //   if (!btns[1].classList.contains("red")) {
-  //     btns[1].classList.add("red");
-  //   } else {
-  //     btns[1].classList.remove("red");
-  //   }
-
   btns[1].classList.toggle("red");
 });
 
-// Ma`lumot classList
-const div = document.createElement("div");
-div.className = "foo";
+/* Deligatsiya */
+// 1-)
+// wrapper.addEventListener("click", (e) => {
+//   if (e.target && e.target.classList.contains("red")) {
+//     console.log("Click me");
+//   }
+// });
+// 1.1-)
 
-// our starting state: <div class="foo"></div>
-console.log(div.outerHTML);
+wrapper.addEventListener("click", (e) => {
+  if (e.target && e.target.matches("button.red")) {
+    console.log("Click me");
+  }
+});
 
-// use the classList API to remove and add classes
-div.classList.remove("foo");
-div.classList.add("anotherclass");
+// btns.forEach((item) => {
+//   item.addEventListener("click", () => {
+//     console.log("Clicked");
+//   });
+// });
 
-// <div class="anotherclass"></div>
-console.log(div.outerHTML);
-
-// if visible is set remove it, otherwise add it
-div.classList.toggle("visible");
-
-// add/remove visible, depending on test conditional, i less than 10
-let i;
-div.classList.toggle("visible", i < 10);
-
-console.log(div.classList.contains("foo"));
-
-// add or remove multiple classes
-div.classList.add("foo", "bar", "baz");
-div.classList.remove("foo", "bar", "baz");
-
-// add or remove multiple classes using spread syntax
-const cls = ["foo", "bar"];
-div.classList.add(...cls);
-div.classList.remove(...cls);
-
-// replace class "foo" with class "bar"
-div.classList.replace("foo", "bar");
+// dinamik tarzda button qo`shish
+const btn = document.createElement("button");
+btn.classList.add("red");
+wrapper.append(btn);
