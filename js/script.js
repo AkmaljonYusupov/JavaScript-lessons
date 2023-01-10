07d0799cfe454b3220481f55cfa851bb7ec89ffe
Akmaljon yusupov <infoakmaljon.uz@gmail.com>
2023-01-10 16:16:26 +0500

    43-dars. Window, doc bilan ishlash

--- js/script.js
+++ js/script.js
@@ -1,100 +1,30 @@
-window.addEventListener("DOMContentLoaded", () => {
-  const tabsParent = document.querySelector(".tabheader__items"),
-    tabs = document.querySelectorAll(".tabheader__item"),
-    tabsContent = document.querySelectorAll(".tabcontent"),
-    loader = document.querySelector(".loader");
+"use strict";
 
-  // loader
-  setTimeout(() => {
-    loader.style.opacity = "0";
-    setTimeout(() => {
-      loader.style.display = "none";
-    }, 500);
-  }, 2000);
-  // function
-  function hideTabContent() {
-    tabsContent.forEach((item) => {
-      item.classList.add("hide");
-      item.classList.remove("show", "fade");
-    });
-    tabs.forEach((item) => {
-      item.classList.remove("tabheader__item_active");
-    });
-  }
-  function showTabContent(i = 0) {
-    tabsContent[i].classList.add("show", "fade");
-    tabsContent[i].classList.remove("hide");
-    tabs[i].classList.add("tabheader__item_active");
-  }
-  hideTabContent();
-  showTabContent();
-  tabsParent.addEventListener("click", (e) => {
-    const target = e.target;
-    if (target && target.classList.contains("tabheader__item")) {
-      tabs.forEach((item, idx) => {
-        if (target == item) {
-          console.log(item);
-          hideTabContent();
-          showTabContent(idx);
-        }
-      });
-    }
-  });
+const box = document.querySelector(".box");
+const buttons = document.querySelector("button");
 
-  //   Timer
-  const deadLine = "2023-02-09";
+// box ichki o`lchami
+// const width = box.clientWidth;
+// const height = box.clientHeight;
+// console.log(width);
+// console.log(height);
 
-  // console.log(Date.parse("2023-02-09"));
+// const width = box.offsetWidth;
+// const height = box.offsetHeight;
 
-  function getTimeRemaining(endtime) {
-    let days, hours, minutes, seconds;
+// console.log(width);
+// console.log(height);
 
-    const timer = Date.parse(endtime) - Date.parse(new Date());
-    if (timer <= 0) {
-      days = 0;
-      hours = 0;
-      minutes = 0;
-      seconds = 0;
-    } else {
-      days = Math.floor(timer / (1000 * 60 * 60 * 24)); // kun
-      hours = Math.floor((timer / (1000 * 60 * 60)) % 24); // soat
-      minutes = Math.floor((timer / 1000 / 60) % 60); // minut
-      seconds = Math.floor((timer / 1000) % 60); // second
-    }
+// const height = box.scrollHeight;
 
-    return { timer, days, hours, minutes, seconds };
-  }
+// console.log(height);
 
-  function getZero(num) {
-    if (num >= 0 && num < 10) {
-      return `0${num}`;
-    } else {
-      return num;
-    }
-  }
-  getZero();
+// buttons.addEventListener("click", () => {
+//   box.style.height = box.scrollHeight + "px";
+//   console.log(box.scrollTop);
+// });
 
-  function setClock(selector, endtime) {
-    const timer = document.querySelector(selector),
-      days = timer.querySelector("#days"),
-      hours = timer.querySelector("#hours"),
-      minutes = timer.querySelector("#minutes"),
-      seconds = timer.querySelector("#seconds"),
-      timeInterval = setInterval(updatClock, 1000);
+// const style = window.getComputedStyle(box);
+// console.log(style.display);
 
-    updatClock();
-
-    function updatClock() {
-      const t = getTimeRemaining(endtime);
-
-      (days.innerHTML = getZero(t.days)),
-        (hours.innerHTML = getZero(t.hours)),
-        (minutes.innerHTML = getZero(t.minutes)),
-        (seconds.innerHTML = getZero(t.seconds));
-      if (t.timer <= 0) {
-        clearInterval(timeInterval);
-      }
-    }
-  }
-  setClock(".timer", deadLine);
-});
+console.log(document.documentElement.clientWidth);
