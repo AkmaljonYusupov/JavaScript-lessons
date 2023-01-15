df6b0fc1c361ddba557545f4fffb2ed538e73252
Akmaljon yusupov <infoakmaljon.uz@gmail.com>
2023-01-15 11:19:05 +0500

    46-dars. Funksiya konstruktor

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
 "use strict";
-console.log("Hello, JavaScript and Mohirdev Akademiy");
+
+// const number = new Function();
+
+// console.dir(number);
+
+function Car(name, color, mph) {
+  this.name = name;
+  this.color = color;
+  this.mph = mph;
+  this.isAirbag = true;
+  this.speed = function () {
+    console.log(`Speed of car ${this.name} is ${this.mph}`);
+  };
+}
+Car.prototype.sayHello = function () {
+  console.log(`Car name of ${this.name} say hello`);
+};
+const bmw = new Car("bmw", "red", 64);
+const merc = new Car("merc", "black", 90);
+bmw.speed();
+merc.speed();
+bmw.sayHello();
+merc.sayHello();
+// console.log(Car.prototype.constructor.length);
+console.log(bmw);
+console.log(merc);
