9fa30d7596974a012a1bc0112a5f6a5dbfde7edf
AKMALJON YUSUPOV <InfoAkmaljon.uz@gmail.com>
2023-02-26 14:33:46 +0500

    49-dars. Class

--- index.js
+++ index.js
@@ -1,14 +1,50 @@
 "use strict";
+// class Car {
+//   constructor(name, color, speed) {
+//     this.name = name;
+//     this.color = color;
+//     this.speed = speed;
+//   }
+//   calcSpeed() {
+//     return this.speed * 100;
+//   }
+// }
+// class Spark extends Car {
+//   constructor(name, color, speed, isAirbag, extraBallon) {
+//     super(name, color, speed);
+//     this.isAirbag = isAirbag;
+//     this.extraBallon = extraBallon;
+//   }
+//   logger() {
+//     console.log(
+//       `Name of Car ${this.name}, color is ${this.color}. Is Airbag ${this.isAirbag}. Are there extra ballon ${this.extraBallon} `
+//     );
+//   }
+// }
+
+// // const bmw = new Car("BMW", "Black", 200);
+// // const merc = new Car("MERC", "Red", 250);
+
+// // console.log(bmw.calcSpeed());
+// // console.log(merc.calcSpeed());
+
+// const aboutcar = new Spark("Spark", "Black", 20, true, 5);
+// aboutcar.logger();
+// console.log(aboutcar.calcSpeed());
+// console.log(aboutcar);
+
 class Car {
   constructor(name, color, speed) {
     this.name = name;
     this.color = color;
     this.speed = speed;
   }
+
   calcSpeed() {
     return this.speed * 100;
   }
 }
+
 class Spark extends Car {
   constructor(name, color, speed, isAirbag, extraBallon) {
     super(name, color, speed);
@@ -17,16 +53,15 @@ class Spark extends Car {
   }
   logger() {
     console.log(
-      `Name of Car ${this.name}, color is ${this.color}. Is Airbag ${this.isAirbag}. Are there extra ballon ${this.extraBallon} `
+      `Name of car ${this.name}, color is ${this.color}. Is air bag there ${this.isAirbag}. Are there extra ballon ${this.extraBallon}`
     );
   }
 }
 
-// const bmw = new Car("BMW", "Black", 200);
-// const merc = new Car("MERC", "Red", 250);
-
+// const bmw = new Car("BMW", "Black", 250);
+// const mers = new Car("MERS", "Red", 300);
 // console.log(bmw.calcSpeed());
-// console.log(merc.calcSpeed());
+// console.log(mers.calcSpeed());
 
 const aboutcar = new Spark("Spark", "Black", 20, true, 5);
 aboutcar.logger();
